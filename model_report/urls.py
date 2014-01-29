f5e00de76363beb78c5a8e9fd58ca428d216b257
Janusz Harkot <jh@blueice.pl>
2014-01-29 22:35:48 +0100

    django 1.5 has depreciated urls.defaults.*

--- model_report/urls.py
+++ model_report/urls.py
@@ -2,7 +2,7 @@
 try:
     from django.conf.urls import patterns, url
 except ImportError:
-    from django.conf.urls.defaults import *
+    from django.conf.urls import *
 
 from model_report.views import report, report_list
 
